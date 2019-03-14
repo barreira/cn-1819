779155391318461e1fc7423bfe2f5e9978bf027e
João Barreira <jpiresbarreira@gmail.com>
2019-03-14 14:52:47 +0000

    Corrigidos pré-processamento e criação do modelo. Adicionada avaliação do modelo.

--- ann.py
+++ ann.py
@@ -2,6 +2,7 @@ import pandas as pd
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report,confusion_matrix
 
 # Read dataset from CSV file
 
@@ -17,12 +18,13 @@ df = df.drop(['BI-RADS'], axis=1)
 
 df = df.dropna()
 
-# Create separate dataframes from features and target
+# Separate features from target
 
-df_features = pd.DataFrame(df.drop('Severity', axis=1).values)  # .values used so that old indexes are discarded
-df_target = pd.DataFrame(df['Severity'].values)  # .values used so that old indexes are discarded
+df_features = pd.DataFrame(df.drop('Severity', axis=1))
+df_target = df['Severity']
 
-print(df_features.head())
+# df_features = pd.DataFrame(df.drop('Severity', axis=1).values)  # .values used so that old indexes are discarded
+# df_target = pd.Series(df['Severity'].values)  # .values used so that old indexes are discarded
 
 # Standardize feature data
 
@@ -42,10 +44,26 @@ columns = [tf.feature_column.numeric_column(key="Age"),
 
 classifier = tf.estimator.DNNClassifier(hidden_units=[10, 20, 10], feature_columns=columns, n_classes=2)
 
-input_func = tf.estimator.inputs.pandas_input_fn(x=X_train, y=y_train, batch_size=20, shuffle=True)
+input_function = tf.estimator.inputs.pandas_input_fn(x=X_train, y=y_train, batch_size=20, shuffle=True)
 
-classifier.train(input_fn=input_func, steps=500)
+classifier.train(input_fn=input_function, steps=500)
 
 # Model evaluation
 
+# Predictions
 
+pred_fn = tf.estimator.inputs.pandas_input_fn(x=X_test,batch_size=len(X_test),shuffle=False)
+
+note_predictions = list(classifier.predict(input_fn=pred_fn))
+
+final_predictions = []
+for pred in note_predictions:
+    final_predictions.append(pred['class_ids'][0])
+
+# Precision Matrix
+
+print(confusion_matrix(y_test, final_predictions))
+
+# Classification Report
+
+print(classification_report(y_test, final_predictions))
