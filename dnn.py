f212f9f36a30393dfa64192bb4d2229f08f3ad40
João Barreira <jpiresbarreira@gmail.com>
2019-03-31 14:29:45 +0100

    Adicionado random search para hiper-parametrização do modelo

--- dnn.py
+++ dnn.py
@@ -1,11 +1,11 @@
 import pandas as pd
+import numpy as np
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.preprocessing import StandardScaler
-
 
 #################################################### PREPROCESSING #####################################################
 
@@ -26,8 +26,8 @@ df = df.dropna()
 
 # Separate features from target
 
-df_features = pd.DataFrame(df.drop('Severity', axis=1))
-df_target = df['Severity']
+features = pd.DataFrame(df.drop('Severity', axis=1))
+target = df['Severity']
 
 # df_features = pd.DataFrame(df.drop('Severity', axis=1).values)  # .values used so that old indexes are discarded
 # df_target = pd.Series(df['Severity'].values)  # .values used so that old indexes are discarded
@@ -35,12 +35,13 @@ df_target = df['Severity']
 # Standardize feature data
 
 scaler = StandardScaler()
-df_features[df_features.columns] = scaler.fit_transform(df_features[df_features.columns])  # scales the existing df
+features[features.columns] = scaler.fit_transform(features[features.columns])  # scales the existing df
 
 
 ####################################################### MODEL ##########################################################
 
 
+# Creates Keras model
 def create_model(hidden_layers=2, nodes_per_layer=3, activation_fn='relu', learning_rate=10e-2):
     model = Sequential()
     model.add(Dense(4, activation=activation_fn, input_shape=(4,)))  # input layer
@@ -56,16 +57,32 @@ def create_model(hidden_layers=2, nodes_per_layer=3, activation_fn='relu', learn
     return model
 
 
+# Prints the top-X (by default = 3) results from model's hyperparameters optimization
+def print_top_results(results, n_top=3):
+    for i in range(1, n_top + 1):
+        candidates = np.flatnonzero(results['rank_test_score'] == i)
+        for candidate in candidates:
+            print("Model with rank: {0}".format(i))
+            print("Mean validation score: {0:.3f} (+/- {1:.3f})".format(results['mean_test_score'][candidate],
+                                                                        results['std_test_score'][candidate]))
+            print("Parameters: {0}\n".format(results['params'][candidate]))
+
+
+# Create Keras classifier model
+
 classifier = KerasClassifier(build_fn=create_model, verbose=0)
 
-# hyperparameters = {
-#     'hidden_layers': [2, 4, 8],
-#     'nodes_per_layer': list(range(1, 21)),
-#     'activation_fn': ['relu', 'sigmoid'],
-#     'learning_rate': [10e-8, 10e-7, 10e-6, 10e-5, 10e-4, 10e-3, 10e-2]
-# }
+# Hyperparameter distribution
 
-# Cross-validation (k = 10 folds) and Results
+hp_dist = {
+    'hidden_layers': [2, 4, 8, 16, 32],
+    'nodes_per_layer': list(range(1, 21)),
+    'activation_fn': ['relu', 'sigmoid'],
+    'learning_rate': [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2]
+}
 
-scores = cross_val_score(classifier, df_features, df_target, cv=10)
-print("Accuracy: %0.5f (+/- %0.5f)" % (scores.mean(), scores.std() * 2))
+# Random Search to optimize hyperparameters + Results
+
+random_search = RandomizedSearchCV(classifier, param_distributions=hp_dist, n_iter=20, cv=10)
+random_search.fit(features, target)
+print_top_results(random_search.cv_results_)
