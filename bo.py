0b14fde9ad6781db3064c54cc292de658afde238
João Barreira <jpiresbarreira@gmail.com>
2019-03-31 21:01:30 +0100

    Adicionada geração de distribuições e descrição do dataset

--- bo.py
+++ bo.py
@@ -23,7 +23,7 @@ class MM:
         self.__x_train, self.__x_test, self.__y_train, self.__y_test = self.mm_data()
         self.__model = self.mm_model()
 
-    # load mnist data from keras dataset
+    # load MM data from keras dataset
     def mm_data(self):
         # Read dataset from CSV file
 
@@ -52,7 +52,7 @@ class MM:
         x_train, x_test, y_train, y_test = train_test_split(df_features, df_target, test_size=0.33, random_state=42)
         return x_train, x_test, y_train, y_test
 
-    # mnist model
+    # MM model
     def mm_model(self):
         model = Sequential()
         model.add(Dense(4, activation=self.activation_fn, input_shape=(4,)))  # input layer
@@ -67,13 +67,13 @@ class MM:
 
         return model
 
-    # fit mnist model
+    # fit MM model
     def mm_fit(self):
         early_stopping = EarlyStopping(patience=0, verbose=1)
 
         self.__model.fit(self.__x_train, self.__y_train, verbose=0, callbacks=[early_stopping])
 
-    # evaluate mnist model
+    # evaluate MM model
     def mm_evaluate(self):
         self.mm_fit()
 
@@ -81,20 +81,20 @@ class MM:
         return evaluation
 
 
-# #### Runner function for the MNIST model
+# #### Runner function for the MM model
 
-# function to run mnist class
+# function to run MM class
 def run_mm(hidden_layers=2, nodes_per_layer=3, activation_fn=0, learning_rate=10e-2):
-    _mnist = MM(hidden_layers=hidden_layers, nodes_per_layer=nodes_per_layer, activation_fn=activation_fn,
-                learning_rate=learning_rate)
-    mnist_evaluation = _mnist.mm_evaluate()
-    return mnist_evaluation
+    _mm = MM(hidden_layers=hidden_layers, nodes_per_layer=nodes_per_layer, activation_fn=activation_fn,
+             learning_rate=learning_rate)
+    mm_evaluation = _mm.mm_evaluate()
+    return mm_evaluation
 
 
 # ## Bayesian Optimization
 # #### bounds for hyper parameters
 
-# bounds for hyper-parameters in mnist model
+# bounds for hyper-parameters in MM model
 # the bounds dict should be in order of continuous type and then discrete type
 bounds = [{'name': 'hidden_layers', 'type': 'discrete', 'domain': (2, 4, 8)},
           {'name': 'nodes_per_layer', 'type': 'discrete', 'domain': tuple(range(1, 21))},
@@ -104,66 +104,64 @@ bounds = [{'name': 'hidden_layers', 'type': 'discrete', 'domain': (2, 4, 8)},
 
 # #### Bayesian Optimization
 
-# function to optimize mnist model
+# function to optimize MM model
 def f(x):
     print(x)
-    evaluation = run_mm(
-        hidden_layers=int(x[:, 0]),
-        nodes_per_layer=int(x[:, 1]),
-        activation_fn=int(x[:, 2]),
-        learning_rate=float(x[:, 3]))
+    evaluation = run_mm(hidden_layers=int(x[:, 0]), nodes_per_layer=int(x[:, 1]), activation_fn=int(x[:, 2]),
+                        learning_rate=float(x[:, 3]))
     print("LOSS:\t{0} \t ACCURACY:\t{1}".format(evaluation[0], evaluation[1]))
     print(evaluation)
-    return evaluation[0]
+    return evaluation[1]
 
 
 # #### Optimizer instance
 
 # optimizer
-opt_mnist = GPyOpt.methods.BayesianOptimization(f=f, domain=bounds)
+opt_mm = GPyOpt.methods.BayesianOptimization(f=f, domain=bounds, maximize=True)
 
 # #### Running optimization
 
-# optimize mnist model
-opt_mnist.run_optimization(max_iter=10)
+# optimize MM model
+opt_mm.run_optimization(max_iter=50)
 
 # #### The output
 
-# print optimized mnist model
+# print optimized MM model
 print("""
 Optimized Parameters:
 \t{0}:\t{1}
 \t{2}:\t{3}
 \t{4}:\t{5}
 \t{6}:\t{7}
-""".format(bounds[0]["name"], opt_mnist.x_opt[0],
-           bounds[1]["name"], opt_mnist.x_opt[1],
-           bounds[2]["name"], opt_mnist.x_opt[2],
-           bounds[3]["name"], opt_mnist.x_opt[3]))
-print("optimized loss: {0}".format(opt_mnist.fx_opt))
+""".format(bounds[0]["name"], opt_mm.x_opt[0],
+           bounds[1]["name"], opt_mm.x_opt[1],
+           bounds[2]["name"], opt_mm.x_opt[2],
+           bounds[3]["name"], opt_mm.x_opt[3]))
+print("optimized accuracy: {0}".format(abs(opt_mm.fx_opt)))
 
 
-def create_model(hidden_layers=2, nodes_per_layer=3, activation_fn=0, learning_rate=10e-2):
-    hidden_layers = int(hidden_layers)
-    nodes_per_layer = int(nodes_per_layer)
-    learning_rate = int(learning_rate)
-    if activation_fn == 0:
-        activation_fn = 'relu'
-    else:
-        activation_fn = 'sigmoid'
-
-    model = Sequential()
-    model.add(Dense(4, activation=activation_fn, input_shape=(4,)))  # input layer
-
-    for i in range(int(hidden_layers)):
-        model.add(Dense(nodes_per_layer, activation=activation_fn))
-
-    model.add(Dense(1, activation=activation_fn))  # output layer
-
-    adam = Adam(lr=learning_rate)
-    model.compile(loss='mean_squared_error', optimizer=adam, metrics=['accuracy'])
-
-    return model
-
-
-plot_model(create_model(opt_mnist.x_opt[0], opt_mnist.x_opt[1], opt_mnist.x_opt[2], opt_mnist.x_opt[3]), to_file='model.png', show_shapes=True)
+# def create_model(hidden_layers=2, nodes_per_layer=3, activation_fn=0, learning_rate=10e-2):
+#     hidden_layers = int(hidden_layers)
+#     nodes_per_layer = int(nodes_per_layer)
+#     learning_rate = int(learning_rate)
+#     if activation_fn == 0:
+#         activation_fn = 'relu'
+#     else:
+#         activation_fn = 'sigmoid'
+#
+#     model = Sequential()
+#     model.add(Dense(4, activation=activation_fn, input_shape=(4,)))  # input layer
+#
+#     for i in range(int(hidden_layers)):
+#         model.add(Dense(nodes_per_layer, activation=activation_fn))
+#
+#     model.add(Dense(1, activation=activation_fn))  # output layer
+#
+#     adam = Adam(lr=learning_rate)
+#     model.compile(loss='mean_squared_error', optimizer=adam, metrics=['accuracy'])
+#
+#     return model
+#
+#
+# plot_model(create_model(opt_mm.x_opt[0], opt_mm.x_opt[1], opt_mm.x_opt[2], opt_mm.x_opt[3]), to_file='model.png',
+#            show_shapes=True)
